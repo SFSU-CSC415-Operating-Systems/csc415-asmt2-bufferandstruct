82111311502f1c988cd1a0223f5baf9d13c9974b
Mark Kim <mkim22@mail.sfsu.edu>
2022-09-07 17:01:41 -0700

    add name to Makefile; start on assignment .c file

---
+++ kim_mark_HW2_main.c
@@ -0,0 +1,19 @@
+/**************************************************************
+* Class:  CSC-415-02 Fall 2022
+* Name: Mark Kim
+* Student ID: 918204214
+* GitHub UserID: mkim797
+* Project: Assignment 2 – Stucture in Memory and Buffering
+*
+* File: kim_mark_HW2_main.c
+*
+* Description:
+*
+**************************************************************/
+#include <stdio.h>
+#include <assignment2.h>
+
+int main ( int argc, char *argv[] ) {
+    struct personalInfo *me = malloc (sizeof(struct personalInfo));
+    
+}
